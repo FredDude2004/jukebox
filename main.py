01480c186bd284508a1c24d57dd572f3bf21e8d5
FredDude2004 <kalebduchesneau@gmail.com>
2025-05-21 19:07:42 -0500

    Changing structure to hopefully have a working database

--- main.py
+++ main.py
@@ -1,12 +1,34 @@
-from flask import Blueprint, render_template
-from . import db
+from flask import Flask, render_template, request, redirect, session, url_for
+from werkzeug.security import generate_password_hash, check_password_hash
+from flask_sqlalchemy import SQLAlchemy
 
-main = Blueprint('main', __name__)
+app = Flask(__name__)
+app.secret_key = '3c33fd3e89db2e03b4fef21e367a705590a89fc6e9b7d483591fd0aaa0e49397'
 
-@main.route('/')
+# Configure SQL Alchemy
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///users.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+
+# Database Model
+
+
+
+
+# Routes
+@app.route('/')
 def index():
+    if "username" in session:
+        return redirect(url_for('dashboard'))
     return render_template('index.html')
 
-@main.route('/profile')
+@app.route('/profile')
 def profile():
     return render_template('profile.html')
+
+
+if __name__ in "__main__":
+    with app.app_context():
+        db.create_all()
+    app.run(debug=True)
+
