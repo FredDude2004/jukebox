bfcbda1590c3fa40c6942a64265cb4b2ce73ec83
FredDude2004 <kalebduchesneau@gmail.com>
2025-05-21 21:50:10 -0500

    Trying to get registration working

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template, request, redirect, session, url_for
+from sqlalchemy import Nullable
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask_sqlalchemy import SQLAlchemy
 
@@ -10,9 +11,18 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///users.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
-### Database Model
+### Database Model ~ Single Row in Databse
+class User(db.Model):
+    # Class Variables
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(25), unique=True, nullable=False)
+    password_hash = db.Column(db.String(250), nullable=False)
 
+    def set_password(self, password):
+        self.password_hash = generate_password_hash(password)
 
+    def check_password(self, password):
+        return check_password_hash(self.password_hash, password)
 
 
 ### Routes
@@ -22,6 +32,49 @@ def index():
         return redirect(url_for('dashboard'))
     return render_template('index.html')
 
+### Login
+@app.route('/login', methods=["POST"])
+def login():
+    # Collect infor from the form
+    username = request.form['username']
+    password = request.form['password']
+    user = User.query.filter_by(username=username).first()
+    if user and user.check_password(password):
+        session['username'] = username
+        return redirect(url_for('dashboard'))
+    else:
+        return render_template('index.html')
+
+
+### Register
+@app.route('/register', methods=['POST'])
+def register():
+    username = request.form['username']
+    password = request.form['password']
+    user = User.query.filter_by(username=username).first()
+    if user:
+        return render_template('index.html', error='User already exists')
+    else:
+        new_user = User(username = username)
+        new_user.set_password(password)
+        db.session.add(new_user)
+        db.session.commit()
+        session['username'] = username
+        return redirect(url_for('dashboard'))
+
+
+### Dashboard
+@app.route("/dashboard")
+def dashboard():
+    if 'username' in session:
+        return render_template('dahsboard.html', username=session['username'])
+    return redirect(url_for('home'))
+
+
+
+
+
+### Logout 
 
 if __name__ in '__main__':
     with app.app_context():
